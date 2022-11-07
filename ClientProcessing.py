cca6b0bbbd75f2ca23e3c5f956e897df2b3c4541
Ефимов Алексей <a.efimov@rdp.ru>
2022-11-07 22:53:02 +0300

    [F] haircut

--- ClientProcessing.py
+++ ClientProcessing.py
@@ -1,34 +1,21 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from typing import Union
 import json
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
-class AdministrativeDistrict(BaseModel):
-  district: str
-
-class Area(BaseModel):
-  id: int
+class Item(BaseModel):
   name: str
 
-class Hui(BaseModel):
-  List[Area]
-
-class ObjectType(BaseModel):
-  objectType: str
-
-class CalculationModel(BaseModel):
-  calculationModel: str
-
-AdministrativeDistricts = List[AdministrativeDistrict]
-Areas = List[Area]
-ObjectTypes = List[ObjectType]
 class Request(BaseModel):
-  AdministrativeDistricts
-  Areas
-  ObjectTypes
-  CalculationModel
+  AdministrativeDistricts: List[str]
+  Area: List[str]
+  objectType: List[str]
+  calculationModel: str
+  minConsumers: str
+  maxConsumers: str
 
 
 class ClientProcessing:
@@ -42,21 +29,22 @@ class ClientProcessing:
   allow_headers=["*"],
   )
   @app.post("/")
-  async def get_data(item: List[Area]):    
+  async def get_data(req: Request):
     #Waits for the request and converts into JSON
-    print(item)
-    result = item.json()
-    response = { "postamats": [] }
-    # with open("TmpData_new.json") as f:
-    #   json_data = json.load(f)
-    # f.close()
-    # print(result)
-    # for area in result:
-    #   for district in json_data["Districts"]:
-    #     for are in district["area"]:
-    #       if area == are:
-    #         response['postamats'].append(are)
-
+    # print(req)
+    # result = item.json()
+    response = { "Postamats": [] }
+    with open("TmpData_new.json") as f:
+      json_data = json.load(f)
+    f.close()
+    for area in req.Area:
+      for district in json_data["Districts"]:
+        for are in district["area"]:
+          if area == are['name']:
+            for i in are['postamats']:
+              print(i)
+              response['Postamats'].append(i)
+    print(response)
     # Prints result in cmd – verification purpose
     return response
 
@@ -72,4 +60,10 @@ class ClientProcessing:
     uvicorn.run(self.app, host=config_data["ip_addr"], port=config_data["port"])
 
 
-        
+# with open("TmpData_new.json") as f:
+#   json_data = json.load(f)
+# f.close()
+
+# for district in json_data["Districts"]:
+#   for are in district["area"]:
+#     print(are['postamats'])
--- readme.md
+++ readme.md
@@ -8,3 +8,4 @@ source venv/bin/activate
 ```
 uvicorn main:app --reload
 ```
+

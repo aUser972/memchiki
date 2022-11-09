619bc5fbcd9f6345597aea17c6ffad4c9cb1d3f5
Ефимов Алексей <a.efimov@rdp.ru>
2022-11-09 23:31:24 +0300

    [F] fix

--- OpenMapData.py
+++ OpenMapData.py
@@ -2,44 +2,44 @@ import json
 import random
 from geopy import Nominatim
 from pymongo import MongoClient
+import openpyxl
+from geopy import Yandex
 
 class OpenMapData:
   def getPostamatData():
-    print("Generate data")
+    geolocator_yandex = Yandex(api_key="1b2779ee-0b47-46e1-a95f-1f45d41033b5")
+    geolocator_nomenatim = Nominatim(user_agent="memchiki")
+    workbook = openpyxl.load_workbook("HouseInfo.xlsx")
+    worksheet = workbook.active
     json_data = {}
-    with open("another.json", "r") as f:
+    with open("another_safe.json", "r") as f:
       json_data = json.load(f)  
-    f.close()    
-    geolocator = Nominatim(user_agent="memchiki")
-
-    lat_from = 55.564566
-    lat_to = 55.915321
-    lon_from = 37.315118
-    lon_to = 37.943093
-    for i in range(200):
-      lat = round(random.uniform(lat_from, lat_to), 6)
-      lon = round(random.uniform(lon_from, lon_to), 6)
-      coef = round(random.uniform(0, 1), 4)
+    f.close()
+    i=1
+    for row in worksheet.iter_rows(2, 300):
       try:
-        addr = geolocator.reverse((lat,lon))
+        location = geolocator_yandex.geocode(row[2].value)
+        coef = round(random.uniform(0, 1), 4)
+        addr = geolocator_nomenatim.reverse((location.latitude,location.longitude))
         for district in json_data["Districts"]:
           for are in district["area"]:
             if addr.address.find(are['name']) > 0:
-              postamat = {"id": i, "lattitude": lat, "longtitude": lon, "address": addr.address, "coefficient": coef}
+              postamat = {"id": i, "lattitude": location.latitude, "longtitude": location.longitude, "address": addr.address, "coefficient": coef}
               are['postamats'].append(postamat)
+              i+=1
       except:
-        print("Exception occured while reverse geocodein, id: ", i)
+        print("Exception occured while reverse geocodein")
 
     with open("TmpData_new.json", "w") as f:
       json.dump(json_data, f, ensure_ascii=False)
     f.close()
 
-    client = MongoClient('localhost', 27017)
-    db = client['postamats_db']
-    db.create_collection("postamats")
-    postamats_collections = db["postamats"]
-    postamats_collections.insert_one(json_data)
-    client.close()
+    # client = MongoClient('localhost', 27017)
+    # db = client['postamats_db']
+    # db.create_collection("postamats")
+    # postamats_collections = db["postamats"]
+    # postamats_collections.insert_one(json_data)
+    # client.close()
 
   def test():
     print("Try test")
@@ -50,26 +50,5 @@ class OpenMapData:
     data = coll.find_one({"Districts.name": "Северо-Западный"})
     print(data)
 
-# OpenMapData.getPostamatData()
-OpenMapData.test()
-
-
-
-
-  # def getDistrictAndArea():
-  #   print("Get district and area")
-
-  # def getTradeShop():
-  #   print("Get trade shop")
-
-  # def getMFC():
-  #   print("Get MFC")
-  
-  # def getLibrary():
-  #   print("Get library")
-
-  # def getCultureHouse():
-  #   print("Get clubs and culture house")
-
-  # def getSportsObjects():
-  #   print("Get sports objects") 
+OpenMapData.getPostamatData()
+# OpenMapData.test()
